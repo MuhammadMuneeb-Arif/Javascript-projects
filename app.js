9b16f72999e0679049c6b5f5755a02694922fb2d
M.MUNEEB AHMED <161030327+MuhammadMuneeb-Arif@users.noreply.github.com>
2024-10-19 21:13:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,85 @@
+
+// var  x  = "muneeb";
+
+// console.log(x);
+
+
+// let y  = 0; 
+
+
+// var Name = "Haider";
+//     console.log("hello", Name + ",", `"would you like to learn some python today? "`);
+
+//   var  personName = " muneeb";
+//     console.log("uppercase:", personName.toUpperCase());
+//     console.log("lowercase:", personName.toLowerCase());
+//     console.log("titlecase:", personName.replace(/\b\w/g, c => c.toUpperCase()));
+
+//     /// 6/10/24;-
+
+
+
+// var className = "BOYS JAVASCRIPT";
+
+
+// ARTHEMATIC OPERATOPRS
+
+var a = "10";
+var b= 200;
+ var c = a-b;
+
+ console.log(c);
+
+ // BODMAS;
+ // PEDMAS;
+  var  unitPrice = 100;
+  var quantity = 2;
+  var totalPrice = unitPrice * quantity;
+  console.log(totalPrice)
+ console.log(true+1, "totalPrice");
+
+ // unary operations:
+//  post fix increment;
+
+
+
+// var a =9;
+// console.log(a>=10);
+
+
+// var b = 10;
+// console.log(b!=10);
+
+var e = 11;
+var d=10;
+ var c;
+ c = (d==10) ||(e==11); 
+
+console.log(c);
+
+
+
+var input = prompt("Enter any number")
+
+if (input == null){
+    console.log("Please enter a number");
+    
+} else {
+    console.log(`${input}`);
+    
+}
+
+
+var moduler= input%2
+var result = (moduler == 1 )&& "odd"
+|| "even"
+
+console.log(result);
+
+//19 -10 -24:=
+
+var car ={
+    name
+}
+
+
